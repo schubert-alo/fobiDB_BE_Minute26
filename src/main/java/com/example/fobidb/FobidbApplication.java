84e3d1354c7d4324e92d338a03c06047f453934f
“simon <simon.schubert@aloberlin.de>
2025-01-24 09:18:32 +0100

    inital commit Tutorial Minute 26

---
+++ src/main/java/com/example/fobidb/FobidbApplication.java
@@ -0,0 +1,13 @@
+package com.example.fobidb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FobidbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FobidbApplication.class, args);
+	}
+
+}
